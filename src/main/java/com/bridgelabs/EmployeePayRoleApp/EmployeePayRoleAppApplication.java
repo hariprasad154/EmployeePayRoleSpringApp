91a5146ff4d0dd53788f12fbcb320538a4c92e0b
hariprasad154 <hariprasas154@gmail.com>
2023-04-11 14:31:52 +0530

    added

---
+++ src/main/java/com/bridgelabs/EmployeePayRoleApp/EmployeePayRoleAppApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabs.EmployeePayRoleApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayRoleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayRoleAppApplication.class, args);
+	}
+
+}
