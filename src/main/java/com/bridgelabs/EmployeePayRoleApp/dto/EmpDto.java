518556f05660785472e5301998c0bc8f99fed088
hariprasad154 <hariprasas154@gmail.com>
2023-04-14 13:47:06 +0530

    added

--- src/main/java/com/bridgelabs/EmployeePayRoleApp/dto/EmpDto.java
+++ src/main/java/com/bridgelabs/EmployeePayRoleApp/dto/EmpDto.java
@@ -1,10 +1,8 @@
 package com.bridgelabs.EmployeePayRoleApp.dto;
 
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.Getter;
 
@@ -19,16 +17,17 @@ public class EmpDto {
     private String name;
     @Min(value = 400,message = "The minumum salary shoud be >400")
     private double salary;
-    @NotEmpty(message = "The gender is not empty")
+    @Pattern(regexp = "male|female|Male|Female",message = "The gender is not empty")
     private String gender;
-    @NotEmpty(message = "The profilepicnot empty")
+    @NotBlank(message = "The profilepicnot empty")
     private  String profilePic;
-
+    @NotEmpty(message = "The depaartment shoud not beempty")
     private List<String> department;
-
+    @JsonFormat(pattern = "dd-MM-yyyy")
     private LocalDate startDate;
-    @NotEmpty(message = "The note shoud be written")
+    @NotBlank(message = "The note shoud be written")
     private String note;
+    @NotEmpty(message = "The Phone number is not empty")
     private List<String> phoneNumber;
 
 
